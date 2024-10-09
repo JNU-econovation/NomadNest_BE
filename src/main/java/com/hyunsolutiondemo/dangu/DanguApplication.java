6f17812da147a33336dcefd4b82b69ccee17d632
Lee Da-hyeon <dlekgus0205@naver.com>
2024-10-10 02:14:47 +0900

    setting: 초기세팅

---
+++ src/main/java/com/hyunsolutiondemo/dangu/DanguApplication.java
@@ -0,0 +1,13 @@
+package com.hyunsolutiondemo.dangu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanguApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanguApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyunsolutiondemo/dangu/DanguApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyunsolutiondemo.dangu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanguApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
